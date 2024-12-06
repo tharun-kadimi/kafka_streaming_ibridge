f08a17579e0947c8fa5e32ca26f87c3da8e9416e
tharun-kadimi <tharunkadimi13@gmail.com>
2024-12-06 18:23:04 +0530

    consumer data is updated

--- Backend/kafka_consumer.py
+++ Backend/kafka_consumer.py
@@ -2,13 +2,21 @@ from flask import Flask, jsonify
 from confluent_kafka import Consumer
 import json
 import threading
-
+import os 
 # Initialize Flask app
-app = Flask(__name__)
-
+# app = Flask(__name__)
+# global messages
+# messages=[]
 # Kafka consumer configuration
 KAFKA_BROKER = 'localhost:9092'
 TOPIC_NAME = 'iB360_access_records'
+output_file = r'C:\Users\tharu\OneDrive\Desktop\ibridge_data\frontend\public\messages.json'
+
+# Create or truncate the file
+# if not os.path.exists("public"):
+#     os.makedirs("public")
+with open(output_file, "w") as f:
+    json.dump([], f)
 
 consumer_config = {
     'bootstrap.servers': KAFKA_BROKER,
@@ -31,6 +39,12 @@ def consume_messages():
 
         try:
             message_value = json.loads(msg.value().decode('utf-8'))
+            # messages.append(message_value)
+            with open(output_file, "r+") as f:
+                data = json.load(f)
+                data.append(message_value)
+                f.seek(0)
+                json.dump(data, f, indent=4)
             print(f"Consumed message: {message_value}")
 
         except json.JSONDecodeError as e:
@@ -43,5 +57,6 @@ consumer_thread = threading.Thread(target=consume_messages, daemon=True)
 consumer_thread.start()
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     consume_messages.run(debug=True)
+consume_messages()
